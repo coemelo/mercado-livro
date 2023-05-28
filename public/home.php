0b04d3e9a68a4b43e0808a3f7f065dd171d0a6e4
Krauss <merlosamuel@protonmail.ch>
2023-05-28 17:08:40 -0300

    Create the main layout

---
+++ public/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/output.css" rel="stylesheet">
+    <title>Mercado Livro Brasil</title>
+</head>
+<body>
+    <header class="bg-[#fff159] w-full p-4">
+        <div class="mb-4">
+            <input type="search" name="search" id="searchBar" placeholder="Search" class="px-2 drop-shadow-md w-10/12">
+            <label for="searchBar" class="bg-white px-2 drop-shadow-md w-1/12">O</label>
+        </div>
+
+        <div><!-- User's Adress --></div>
+        <nav class="flex flex-row text-xs justify-between text-gray-500">
+            <ul class="mx-2">
+                <li>Categories</li>
+            </ul>
+            <ul class="mx-2">
+                <li>Daily Offers</li>
+            </ul>
+            <ul class="mx-2">
+                <li>History</li>
+            </ul>
+            <ul class="mx-2">
+                <li>Sell</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class=" min-h-screen py-2 px-1">
+
+    <p>So sad, no? There's nothing to see here! ;&#40</p>
+
+    </main>
+
+    <footer class="w-full text-xs flex flex-col p-2 border-t-2">
+        <div>
+            <p class=" font-bold">Github</p>
+        </div>
+        <p>Copyright © 2023 Coemelo.</p>
+    </footer>
+</body>
+</html>
